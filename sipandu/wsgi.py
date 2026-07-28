b473fed23834e950dc13f825610e1cf63e7cf3bf
AhmadGhozi <ahmadghozi1806@gmail.com>
2026-07-28 10:05:21 +0800

    Rename folder project dari sipadu ke sipandu

--- sipandu/wsgi.py
+++ sipandu/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sipadu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sipandu.settings')
 
 application = get_wsgi_application()
